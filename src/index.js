d23afbc3fccf8faaf3186eb4a76ad792b2fddea9
Caerdroia <denis.voronin@gmail.com>
2022-02-02 13:13:30 +0300

    feat: sum division implementation begin

--- src/index.js
+++ src/index.js
@@ -9,13 +9,29 @@ module.exports = function check(str, bracketsConfig) {
 
     bracketsConfig = ((bracketsConfig.length % 2) ? false : true);
 
+
     str = str.split('');
 
-    console.log(typeof str)
+    const str2 = ((str.length % 2) ? false : true);
 
-    str = ((str.length % 2) ? false : true);
+    let sum1 = 0;
+    let sum2 = 0;
+    for (i = 0; j = str.length, i < j; i++) {
 
-    isTrue = ((str === true) && (bracketsConfig === true)) ? true : false;
+        // добавляем в каждой цифре '1', 
+        // чтобы при делении одной суммы на другую не делить на ноль
+
+        if (str[i] === '(') {
+            sum1 += i + 1;
+
+        } else if (str[i] === ')') {
+            sum2 += i + 1;
+        }
+    }
+    str3 = ((sum2 % sum1) ? true : false);
+
+
+    isTrue = ((str2 === true) && (str3 === true) && (bracketsConfig === true)) ? true : false;
 
     return isTrue;
 }
