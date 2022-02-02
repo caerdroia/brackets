6bd42a60d5b58e05ba29f18bee58277a0af263c6
Caerdroia <denis.voronin@gmail.com>
2022-02-02 11:49:00 +0300

    feat: basic odd/even check implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    bracketsConfig = bracketsConfig
+        .join()
+        .split('')
+        .filter((element) => element === ',' ? '' : 'element');
+
+    console.log(typeof bracketsConfig);
+
+    bracketsConfig = ((bracketsConfig.length % 2) ? false : true);
+
+    str = str.split('');
+
+    console.log(typeof str)
+
+    str = ((str.length % 2) ? false : true);
+
+    isTrue = ((str === true) && (bracketsConfig === true)) ? true : false;
+
+    return isTrue;
 }
